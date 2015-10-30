0a31def5230dbfd6047ff231d66bbcda6fc7639c
stiliyan vukadinov <stiliyan.vukadinov@maniaprint.bg>
2015-10-30 18:07:53 +0200

    first commit

---
+++ FirmClubPrinters.dpr
@@ -0,0 +1,13 @@
+program FirmClubPrinters;
+
+uses
+  Forms,
+  MainUnit in 'D:\Delphi7Projects\Megalan\FirmClubPrinters\MainUnit.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
